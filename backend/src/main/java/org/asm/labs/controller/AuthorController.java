a57f98998caa2a8a1549322d024aac90526d17b5
Юрий Маслов <drunkmowgli@gmail.com>
2019-11-05 20:54:49 +0300

    Working on 201. Maybe done it, maybe no ;)

--- backend/src/main/java/org/asm/labs/controller/AuthorController.java
+++ backend/src/main/java/org/asm/labs/controller/AuthorController.java
@@ -11,12 +11,15 @@ package org.asm.labs.controller;
 import org.asm.labs.domain.Author;
 import org.asm.labs.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @RestController
 public class AuthorController {
@@ -27,10 +30,14 @@ public class AuthorController {
     public AuthorController(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
     }
-
+    
+    //    @PostMapping("/api/v1/authors")
+//    public Mono<Author> create(@RequestBody Author author) {
+//        return authorRepository.save(author);
+//    }
     @PostMapping("/api/v1/authors")
-    public Mono<Author> create(@RequestBody Author author) {
-        return authorRepository.save(author);
+    public Mono<ResponseEntity> create(@RequestBody Author author) {
+        return authorRepository.save(author).map(a -> ResponseEntity.created(URI.create("/api/v1/authors/" + a.getId())).build());
     }
     
     @GetMapping("/api/v1/authors")
