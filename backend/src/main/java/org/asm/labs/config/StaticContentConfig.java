d8cc5c5226c17051da9323ebd25f0c5ab68b7011
Юрий Маслов <drunkmowgli@gmail.com>
2019-11-21 00:24:54 +0300

    Add Static Config and work on REDIRECT API.

---
+++ backend/src/main/java/org/asm/labs/config/StaticContentConfig.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019.
+ *
+ * Created by drunkmowgli on 20/11/2019
+ *
+ * @author drunkmowgli
+ */
+
+package org.asm.labs.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Configuration
+public class StaticContentConfig {
+    
+    @Bean
+    public RouterFunction<ServerResponse> htmlRouter(@Value("classpath:/public/index.html") Resource html) {
+        return route(
+            GET("/"),
+            request -> ok()
+                .contentType(MediaType.TEXT_HTML)
+                .syncBody(html)
+        );
+    }
+    
+    @Bean
+    public RouterFunction<ServerResponse> imgRouter() {
+        return RouterFunctions.resources("/img/**", new ClassPathResource("img/"));
+    }
+    
+    @Bean
+    RouterFunction<ServerResponse> redirect() {
+        return route(GET("/books/**"), req ->
+            ServerResponse.temporaryRedirect(URI.create("/"))
+                          .build());
+    }
+    
+}
